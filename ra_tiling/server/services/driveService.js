01319395969046f9eba20b4dcd459734891e16d9
callumacourt <callumacourtt@gmail.com>
2025-09-06 15:38:14 +0100

    test: add driveService unit tests

--- ra_tiling/server/services/driveService.js
+++ ra_tiling/server/services/driveService.js
@@ -1,12 +1,16 @@
 import { drive } from "../config/drive.js";
-// Lists all files in the googleDrive folder for the gallery
-async function listFiles(folderId) {
+
+// Lists all image files in the googleDrive folder for the gallery
+export async function listFiles(folderId) {
     try {
         const res = await drive.files.list({
             q: `'${folderId}' in parents and trashed = false`,
             fields: "files(id, name, mimeType)",
         });
-        return res.data.files;
+        // Return empty array if drive is empty, filter drive for only image files
+        return (res.data.files || []).filter(
+            file => file.mimeType && file.mimeType.startsWith('image/')
+        );
     } catch (error) {
         console.log(error)
         throw error;
@@ -15,5 +19,3 @@ async function listFiles(folderId) {
 
 const files = await listFiles(`1h9itLF-gu_Bl2zHtBPB_HPy-HhAtW6S-`);
 console.log(files);
-
-export {};
